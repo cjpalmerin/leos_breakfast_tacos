f8e1c8d29e9e84bfbce66dea89906d7984104289
CJPALME <cjpalmerin@gmail.com>
2020-08-18 13:41:40 -0500

    added jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 
 import { Navbar } from './Components/Navbar/Navbar'
+import { Jumbotron } from './Components/Jumbotron/Jumbotron'
+import { About } from './Components/About/About'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Jumbotron />
+      <About />
     </div>
   );
 }
---
+++ client/src/Components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Jumbotron.css';
+
+import Background from '../../images/photo-1579888944880-d98341245702.jpeg'
+
+export class Jumbotron extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <img src={Background} className='jumboimage' alt='leos breakfast tacos' />
+            </div>
+        )
+    }
+}
