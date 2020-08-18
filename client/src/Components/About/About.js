76f82cba4045dee399b4856dd23b9167ad0a1a75
CJPALME <cjpalmerin@gmail.com>
2020-08-18 13:52:53 -0500

    added about section

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -5,7 +5,7 @@ export class About extends React.Component {
     render() {
         return (
             <div className="about">
-                <h2>About LEOS</h2>
+                <h2>About <span class='italics'> LEOS </span></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna 
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
@@ -13,6 +13,12 @@ export class About extends React.Component {
                     in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
                     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
                     qui officia deserunt mollit anim id est laborum.</p>
+                    <br />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna. 
+                    <br />
+                    <br />
+                    <a>Read the Full Story Here &gt;</a></p>
             </div>
         )
     }
